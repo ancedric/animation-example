6892ad949f21ad71da5576cb09617b4d7f29bbcd
Magnus Junghard Huttu <magnushuttu@gmail.com>
2024-03-27 20:07:16 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+window.addEventListener('scroll', function () {
+  var element = document.querySelector('.scroll-element')
+  var position = element.getBoundingClientRect()
+
+  // checking whether fully visible
+  if (position.top < window.innerHeight && position.bottom >= 0) {
+    element.classList.add('visible')
+  } else {
+    element.classList.remove('visible')
+  }
+})
