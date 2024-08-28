74c7e3308bffa231132b8317ef180d39599a3f6b
ancedric <101697896+ancedric@users.noreply.github.com>
2024-08-28 16:16:12 +0200

    modification of the exercise
    My own contribution to the exercice

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-window.addEventListener('scroll', function () {
+/*window.addEventListener('scroll', function () {
   var element = document.querySelector('.scroll-element')
+  var position = element.getBoundingClientRect()
+
+  // checking whether fully visible
+  if (position.top < window.innerHeight && position.bottom >= 0) {
+    element.classList.add('visible')
+  } else {
+    element.classList.remove('visible')
+  }
+})*/
+
+window.addEventListener('scroll', function () {
+  var element = document.querySelector('.section-2-h1')
+  var position = element.getBoundingClientRect()
+
+  // checking whether fully visible
+  if (position.top < window.innerHeight && position.bottom >= 0) {
+    element.classList.add('visible')
+  } else {
+    element.classList.remove('visible')
+  }
+})
+
+window.addEventListener('scroll', function () {
+  var element = document.querySelector('.section-2-img')
   var position = element.getBoundingClientRect()
 
   // checking whether fully visible
